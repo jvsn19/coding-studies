f9500cee2a4886fca3e6d1ba65d66968d6edc631
Joao Vasconcelos <jvsn19@gmail.com>
2025-01-18 08:54:56 +0000

    add 347

---
+++ leetcode/questions/347/java/347.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        for (int num: nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        count.forEach((key, val) -> {
+            if (heap.size() < k) {
+                heap.offer(new int[] {key, val});
+            } else if (heap.peek()[1] < val) {
+                heap.poll();
+                heap.offer(new int[] {key, val});
+            }
+        });
+
+        int heapSize = heap.size();
+        int[] ans = new int[heap.size()];
+
+        for (int i = 0; i < heapSize; ++i) {
+            int[] pair = heap.poll();
+            ans[i] = pair[0];
+        }
+
+        return ans;
+    }
+}
