d7f2cd157f3e646d2af94ac766fa1dc3b57732f2
Joao Vasconcelos <jvsn19@gmail.com>
2025-02-13 22:12:02 +0000

    fix 234

--- leetcode/questions/234/python/234.py
+++ leetcode/questions/234/python/234.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         def get_mid(node):
